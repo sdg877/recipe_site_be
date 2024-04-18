2364964cc2c12c195a44927e45154aff907ede64
Sdg877 <sdrakegill@gmail.com>
2024-04-18 08:35:21 +0100

    project set up

--- server.js
+++ server.js
@@ -11,8 +11,32 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/recipeDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.DATABASE_URL)
+
+const User = mongoose.model('User', userSchema)
+
+app.post('/user/login', async (req, res) => {
+    const now = new Date()
+
+    if( await User.countDocuments({"userEmail": req.body.userEmail}) === 0) {
+        const newUser = new User ({
+            userEmail: req.body.userEmail,
+            lastLogin: now,
+            uniqueSub: req.body.uniqueSub
+        })
+        newUser.save()
+        .then(() => {
+            res.sendStatus(200)
+        })
+        .catch(err => {
+            res.sendStatus(500)
+        })
+    } else {
+        await User.findOneAndUpdate({"userEmail": req.body.userEmail}, {lastLogin: now}, {"uniqueSub": req.body.uniqueSub})
+        res.sendStatus
+    }
+})
+
+app.use('/api/users', userRoutes);
+
+module.exports = app;
---
+++ src/controllers/userController.jsx
@@ -0,0 +1,22 @@
+// controllers/userController.js
+
+const UserController = {
+    login: (req, res) => {
+      // Controller logic for handling user login
+    },
+    register: (req, res) => {
+      // Controller logic for handling user registration
+    },
+    getProfile: (req, res) => {
+      // Controller logic for getting user profile
+    },
+    updateProfile: (req, res) => {
+      // Controller logic for updating user profile
+    },
+    deleteProfile: (req, res) => {
+      // Controller logic for deleting user profile
+    }
+  };
+  
+  module.exports = UserController;
+  
