8c5b1bd670e724253c6cc32e91bb7efb2862ebcf
Sdg877 <sdrakegill@gmail.com>
2024-06-18 12:43:36 +0100

    bug fixes

--- server.cjs
+++ server.cjs
@@ -1,39 +1,4 @@
-// import 'dotenv/config';
-// import express from 'express';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
-// import mongoose from 'mongoose';
-
-// import userRoutes from './src/routes/users.js';
-// import './config/database.js';
-// import checkToken from './config/checkToken.js';
-
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(express.json())
-
-// mongoose.connect(process.env.DATABASE_URL);
-
-// app.use(checkToken);
-
-// app.get('/', (req, res) => {
-//   console.log('recipe');
-//   res.sendStatus(200);
-// });
-
-// app.use('/api/users', userRoutes);
-
-
-
-// app.listen(port, () => {
-//   console.log(`Server Listening at http://localhost:${port}`);
-// });
-require('dotenv').config(); // Use require instead of import
-
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
