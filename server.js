6dd39b24d5f849e8715e44a9b69ac279cd69fefb
Sdg877 <sdrakegill@gmail.com>
2024-06-18 12:33:43 +0100

    fixing deployment issues

--- server.js
+++ server.js
@@ -32,13 +32,12 @@
 // app.listen(port, () => {
 //   console.log(`Server Listening at http://localhost:${port}`);
 // });
+require('dotenv').config(); // Use require instead of import
 
-import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import path from 'path'; // Import path module
 
 import userRoutes from './src/routes/users.js';
 import './config/database.js';
@@ -49,7 +48,6 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json());
 
 mongoose.connect(process.env.DATABASE_URL);
 
@@ -61,14 +59,6 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/users', userRoutes);
-
-// Serve static files from the build directory (assuming your frontend build output directory is 'build')
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Catch-all endpoint to redirect to your frontend URL
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Server Listening at http://localhost:${port}`);
