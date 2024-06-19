350be8b55cfa773cc6e9e9a70fa5f5dedb42ec17
Sdg877 <sdrakegill@gmail.com>
2024-06-19 16:36:45 +0100

    updating routes

--- server.mjs
+++ server.mjs
@@ -57,18 +57,19 @@ app.use(checkToken);
 // Serve static files from the public directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 // API routes
 app.use('/api/users', userRoutes);
 
 // Catchall handler to serve index.html for all other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/public', 'index.html'));
 });
 
 app.listen(port, () => {
   console.log(`Server Listening at http://localhost:${port}`);
 });
+
 
 
