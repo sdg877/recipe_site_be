dc29f12508f77d6c4ea1509bb0ee2b5e4bfee3d9
Sdg877 <sdrakegill@gmail.com>
2024-06-20 12:29:39 +0100

    redirect path fix

--- server.mjs
+++ server.mjs
@@ -1,41 +1,8 @@
-// import 'dotenv/config';
-// import express from 'express';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
-// import mongoose from 'mongoose';
-
-// import userRoutes from './src/routes/users.js';
-// import './config/database.js';
-// import checkToken from './config/checkToken.js';
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// mongoose.connect(process.env.DATABASE_URL);
-
-// app.use(checkToken);
-
-// app.get('/', (req, res) => {
-//   console.log('recipe');
-//   res.sendStatus(200);
-// });
-
-// app.use('/api/users', userRoutes);
-
-// app.listen(port, () => {
-//   console.log(`Server Listening at http://localhost:${port}`);
-// });
-
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 import userRoutes from './src/routes/users.js';
 import './config/database.js';
@@ -44,34 +11,20 @@ import checkToken from './config/checkToken.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware setup
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.DATABASE_URL);
 
-// Token check middleware
 app.use(checkToken);
 
-// Serve static files from the React app's build folder
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-// API routes
-app.use('/api/users', userRoutes);
-
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+app.get('/', (req, res) => {
+  console.log('recipe');
+  res.sendStatus(200);
 });
+
+app.use('/api/users', userRoutes);
 
 app.listen(port, () => {
   console.log(`Server Listening at http://localhost:${port}`);
 });
-
-
